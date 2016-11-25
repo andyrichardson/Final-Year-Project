ab3e4da348d68fd1124024192dea35ccb5131e02
Andy Richardson <contact@andythedeveloper.com>
2016-11-25 12:25:20 +0000

    remove unneeded sleep function

--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,5 @@
 const assert = require("assert");
 const request = require("request");
-const sleep = require("sleep").sleep;
 const child_process = require("child_process");
 
 describe("Containers", function(){
@@ -22,24 +21,6 @@ describe("Containers", function(){
     });
   });
 
-  describe("Web Server", function(){
-    before(function(done){
-      this.timeout(90000);
-      child_process.execFile('../../dist/web.sh', ["-t"], function(err, stdout, stdin){
-        assert.equal(err, undefined);
-        done();
-      });
-    });
-
-    it("Web server is active", function(done){
-      request("http://localhost:7474", function(err, response){
-        assert.equal(err, undefined);
-        assert.equal(response.statusCode, 200);
-        done();
-      });
-    });
-  });
-
   describe("REST API Server", function(){
     before(function(done){
       this.timeout(90000);
@@ -51,6 +32,24 @@ describe("Containers", function(){
 
     it("API server is active", function(done){
       request("http://localhost:3000/api/test", function(err, response){
+        assert.equal(err, undefined);
+        assert.equal(response.statusCode, 200);
+        done();
+      });
+    });
+  });
+
+  describe("Web Server", function(){
+    before(function(done){
+      this.timeout(90000);
+      child_process.execFile('../../dist/web.sh', ["-t"], function(err, stdout, stdin){
+        assert.equal(err, undefined);
+        done();
+      });
+    });
+
+    it("Web server is active", function(done){
+      request("http://localhost:7474", function(err, response){
         assert.equal(err, undefined);
         assert.equal(response.statusCode, 200);
         done();
