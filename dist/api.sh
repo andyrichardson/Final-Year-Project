3b9e93125550e0206d9f8b2480ca23186a157209
Andy Richardson <contact@andythedeveloper.com>
2016-11-25 11:17:07 +0000

    Testing for API

--- dist/api.sh
+++ dist/api.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Help prompt
 print_help(){
@@ -15,7 +16,6 @@ run_dev(){
 	# Build container
 	docker build -t fyp-api:dev -f dev/api/Dockerfile .
 
-	echo "here"
 	# Kill any previous containers
 	docker rm fyp-api-dev 2> /dev/null
 
@@ -25,6 +25,22 @@ run_dev(){
 	--name=fyp-api-dev \
 	--net=fyp-network \
 	fyp-api:dev
+}
+
+run_test(){
+	# Build container
+	docker build -t fyp-api:test -f $CWD/release/api/Dockerfile $CWD
+
+	# Kill any previous containers
+	docker kill fyp-api-test 2> /dev/null
+	docker rm fyp-api-test 2> /dev/null
+
+	# Run container
+	docker run -p 3000:80 \
+	-v $CWD/../src/api/:/var/www/api \
+	--name=fyp-api-test \
+	--net=fyp-network \
+	fyp-api:test
 }
 
 # Run release container
@@ -41,6 +57,10 @@ case $1 in
 
 	-d | --dev)
 		run_dev
+		;;
+
+	-t | --test)
+		run_test
 		;;
 
 	"")
--- src/test/test.js
+++ src/test/test.js
@@ -3,43 +3,69 @@ const request = require("request");
 const sleep = require("sleep").sleep;
 const child_process = require("child_process");
 
-describe("Database server", function(){
-  before(function(done){
-    this.timeout(30000);
-    child_process.execFile("../../dist/neo4j.sh", ["-t"], function(err, stdout, stdin){
-      assert(err == undefined);
-    });
-    setTimeout(done, 20000);
-  })
-
-  it("Graphing database server is active", function(done){
-    request("http://localhost:7474/", function(err, response){
-      assert(err == undefined);
-      assert(response.statusCode == 200);
-      done();
+describe("Containers", function(){
+  describe("Database server", function(){
+    before(function(done){
+      this.timeout(30000);
+      child_process.execFile("../../dist/neo4j.sh", ["-t"], function(err, stdout, stdin){
+        assert(err == undefined);
+      });
+      setTimeout(done, 20000);
     })
-  });
-});
 
-describe("Web Server", function(){
-  before(function(done){
-    this.timeout(10000);
-    child_process.execFile('../../dist/web.sh', ["-t"], function(err, stdout, stdin){
-      assert(err == undefined);
-      done();
+    it("Graphing database server is active", function(done){
+      request("http://localhost:7474/", function(err, response){
+        assert(err == undefined);
+        assert(response.statusCode == 200);
+        done();
+      })
     });
-    setTimeout(done, 1000);
   });
 
-  it("Web server is active", function(done){
-    request("http://localhost:7474", function(err, response){
-      assert.equal(err, undefined);
-      assert.equal(response.statusCode, 200);
-      done();
-    })
-  })
-});
+  describe("Web Server", function(){
+    before(function(done){
+      this.timeout(10000);
+      child_process.execFile('../../dist/web.sh', ["-t"], function(err, stdout, stdin){
+        assert(err == undefined);
+        done();
+      });
+      setTimeout(done, 1000);
+    });
 
-describe("REST API Server", function(){
+    it("Web server is active", function(done){
+      request("http://localhost:7474", function(err, response){
+        assert.equal(err, undefined);
+        assert.equal(response.statusCode, 200);
+        done();
+      });
+    });
+  });
 
+  describe("REST API Server", function(){
+    before(function(done){
+      this.timeout(10000);
+      child_process.execFile('../../dist/api.sh', ['-t'], function(err, stdout, stdin){
+        assert(err == undefined);
+        done();
+      });
+      setTimeout(done, 6000);
+    });
+
+    it("API server is active", function(done){
+      request("http://localhost:3000/api/test", function(err, response){
+        assert.equal(err, undefined);
+        assert.equal(response.statusCode, 200);
+        done();
+      });
+    });
+
+    it("Web server redirects to API", function(done){
+      request("http://localhost/api/test", function(err, response){
+        assert.equal(err, undefined);
+        assert.equal(response.request.uri.port, 3000);
+        assert.equal(response.statusCode, 200);
+        done();
+      })
+    });
+  });
 });
