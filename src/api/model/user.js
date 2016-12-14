f239e9163e15148088b54e01932925497c5f9339
Andy Richardson <contact@andythedeveloper.com>
2016-12-14 17:34:35 +0000

    get user returns friends also

--- src/api/model/user.js
+++ src/api/model/user.js
@@ -5,70 +5,88 @@ const Token = require('./token');
 
 /* SCHEMA */
 const schema = {
-    username: {
-        type: String,
-        lowercase: true,
-        required: true
-    },
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        lowercase: true,
-        required: true
-    }
+  username: {
+    type: String,
+    lowercase: true,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    required: true
+  }
 };
 
 /* VALIDATION */
 const validate = {
-    all: function(user, callback){
-        console.log('validating');
-        console.log(user);
-        callback();
-    },
-    firstName: function(){
+  all: function(user, callback){
+    console.log('validating');
+    console.log(user);
+    callback();
+  },
+  firstName: function(){
 
-    }
+  }
 };
 
 /* CREATE INSTANCE OF MODEL HANDLER */
 const UserHandler = new ModelHandler('User', schema);
 let model;
 
+const hidePrivateData = function(user){
+  console.log('call');
+  return new Prom(function(fulfill, reject){
+    if(user.friends != undefined){
+      user.friends.forEach(function(el){
+        el.password = undefined;
+        el.email = undefined;
+        return hidePrivateData(el);
+      })
+    }
+
+    user.password = undefined;
+    user.email = undefined;
+    fulfill(user);
+  })
+
+}
+
 /* INITIALIZE */
 module.exports.init = function(db){
-    UserHandler.init(db);
-    model = Prom.promisifyAll(UserHandler.getModel(), {suffix: 'Prom'});
-    model.setUniqueKey('username');
-    model.on('validate', validate.all);
-    model.compose(model, "friends", "has_friend", {many: true});
-    model.compose(model, "requests", "has_request", {many:true});
+  UserHandler.init(db);
+  model = Prom.promisifyAll(UserHandler.getModel(), {suffix: 'Prom'});
+  model.setUniqueKey('username');
+  model.on('validate', validate.all);
+  model.compose(model, "friends", "has_friend", {many: true});
+  model.compose(model, "requests", "has_request", {many:true});
 };
 
 /* LOG IN */
 module.exports.login = function(username, password){
-    return Token.create(username, password);
+  return Token.create(username, password);
 };
 
 /* LOG OUT */
 module.exports.logout = function(token){
-    return Token.delete(token);
+  return Token.delete(token);
 };
 
 /* REGISTER/CREATE */
 module.exports.create = function(obj){
   const user = {
-      username: obj.username,
-      password: obj.password,
-      firstName: obj.firstName,
-      lastName: obj.lastName,
-      email: obj.email
+    username: obj.username,
+    password: obj.password,
+    firstName: obj.firstName,
+    lastName: obj.lastName,
+    email: obj.email
   };
 
   return model.saveProm(user)
@@ -79,55 +97,63 @@ module.exports.create = function(obj){
 
 /* EDIT */
 module.exports.edit = function(username, changes){
-    return model.whereProm({username: username}, {limit: 1})
-    .then(function(node){
-        const user = node[0];
+  return model.whereProm({username: username}, {limit: 1})
+  .then(function(node){
+    const user = node[0];
 
-        // Set new properties
-        for(let attr in user){
-            if(changes.hasOwnProperty(attr)){
-                if(attr == 'username' || attr == 'id'){
-                    continue;
-                }
-
-                user[attr] = changes[attr];
-            }
+    // Set new properties
+    for(let attr in user){
+      if(changes.hasOwnProperty(attr)){
+        if(attr == 'username' || attr == 'id'){
+          continue;
         }
 
-        return model.updateProm(user, true);
-    })
-    .then(function(data){
-        return data;
-    });
+        user[attr] = changes[attr];
+      }
+    }
+
+    return model.updateProm(user, true);
+  })
+  .then(function(data){
+    return data;
+  });
 };
 
 /* CHANGE PASSWORD */
 module.exports.changePassword = function(username, oldPassword, newPassword){
-    // Verify existing password
-    return Token.create(username, oldPassword)
-    .then(function(){
-        // Set new password
-        return Token.password(username, newPassword);
-    })
-    .then(function(){
-        // Get new access token
-        return Token.create(username, newPassword);
-    });
+  // Verify existing password
+  return Token.create(username, oldPassword)
+  .then(function(){
+    // Set new password
+    return Token.password(username, newPassword);
+  })
+  .then(function(){
+    // Get new access token
+    return Token.create(username, newPassword);
+  });
 };
 
 /* GET */
 module.exports.getUser = function(username){
-    return model.whereProm({username: username}, {limit: 1})
-    .then(function(node){
-        if(node[0] === undefined){
-            throw new Error("No such user");
-        }
+  let node;
 
-        const user = node[0];
-        user.password = undefined;
-        user.email = undefined;
-        return user;
-    });
+  return model.whereProm({username: username}, {limit: 1})
+  .then(function(data){
+    node = data;
+    if(node[0] === undefined){
+      throw new Error("No such user");
+    }
+
+    return model.queryProm("MATCH (x:User {username: {username}})-[:has_friend]-(node:User)", {username: username})
+  })
+  .then(function(data){
+    const user = node[0];
+    user.friends = data;
+    user.password = undefined;
+    user.email = undefined;
+
+    return hidePrivateData(user)
+  });
 };
 
 /* SEARCH */
@@ -193,8 +219,7 @@ module.exports.addUser = function(user, friend){
   });
 }
 
-
 /* RETURN MODEL */
 module.exports.model = function(){
-    return model;
+  return model;
 };
