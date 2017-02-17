e58d298f639645bc45aa723c989830478522a257
Andy Richardson <contact@andythedeveloper.com>
2017-02-17 16:47:24 +0000

    allow api middleware in testing

--- src/web/app/includes/api.js
+++ src/web/app/includes/api.js
@@ -1,6 +1,14 @@
 const Prom = require('bluebird');
 const request = Prom.promisifyAll(require('request'), {suffix: "Prom"});
-const baseUri = `http://${window.location.hostname}:3000/api`;
+let baseUri;
+
+if(typeof window == "undefined"){
+  // For testing purposes only
+  baseUri = "http://localhost:3000/api";
+}
+else{
+  baseUri = `http://${window.location.hostname}:3000/api`;
+}
 
 /* USER REGISTRATION */
 module.exports.register = function(data){
